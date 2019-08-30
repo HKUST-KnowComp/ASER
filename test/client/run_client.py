f1640ddb61d00bdef42b91028efff5e8e07d4389
scarletpan <myscarlet@sina.com>
2019-08-30 16:27:48 +0800

    test(c/s): Modify test code of Client/Server

--- test/client/run_client.py
+++ test/client/run_client.py
@@ -4,6 +4,18 @@ from aser.client import ASERClient
 
 if __name__ == "__main__":
     client = ASERClient(port=8000)
-    for i in tqdm(range(10000000)):
-        client.extract_eventualities("the dog barks")
-        pass
+
+    s1 = 'they yucca look leggy'
+    event1 = client.extract_eventualities(s1)
+    s2 = 'they african violet refuse to flower'
+    event2 = client.extract_eventualities(s2)
+    e1 = client.get_exact_match_event(event1)
+    e2 = client.get_exact_match_event(event2)
+    print("Event 1: ")
+    print(e1)
+    print("Event 2: ")
+    print(e2)
+    print("Relation: ")
+    rel = client.get_exact_match_relation(e1, e2)
+    print(rel)
+
--- test/server/run_server.py
+++ test/server/run_server.py
@@ -3,6 +3,7 @@ from aser.server import ASERServer
 if __name__ == "__main__":
     server = ASERServer(
         port=8000,
+        db_path="/home/hjpan/cache/KG.tiny.db",
         corenlp_path="/home/software/stanford-corenlp/stanford-corenlp-full-2018-02-27/",
         base_corenlp_port=9000,
-        corenlp_num=5)
+        corenlp_num=2)
