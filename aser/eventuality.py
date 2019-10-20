975afc6384f4457d6ce3e43efbc98b923a182558
scarletpan <myscarlet@sina.com>
2019-10-20 20:09:20 +0800

    refactor(eventuality): Add `JsonSerilizableObject`

--- aser/eventuality.py
+++ aser/eventuality.py
@@ -1,13 +1,15 @@
 import hashlib
 import json
 import pprint
+from aser.base import JsonSerializedObject
 from aser.extract.utils import sort_dependencies_position, extract_indices_from_dependencies
 
 
-class Eventuality(object):
+class Eventuality(JsonSerializedObject):
     def __init__(self, pattern=None, dependencies=None,
                  skeleton_dependencies=None,
                  sent_parsed_results=None):
+        super().__init__()
         self.eid = None
         self.pattern = pattern
         self._dependencies = None
@@ -132,28 +134,6 @@ class Eventuality(object):
         self._verbs = [i for i, tag in enumerate(self.pos_tags) if tag.startswith('VB')]
 
         self.eid = self._generate_eid(self.words)
-
-    def encode(self, encoding="utf-8"):
-        if encoding == "utf-8":
-            msg = json.dumps(self.__dict__).encode("utf-8")
-        elif encoding == "ascii":
-            msg = json.dumps(self.__dict__).encode("ascii")
-        else:
-            msg = self.__dict__
-        return msg
-
-    def decode(self, msg, encoding="utf-8"):
-        if encoding == "utf-8":
-            decoded_dict = json.loads(msg.decode("utf-8"))
-        elif encoding == "ascii":
-            decoded_dict = json.loads(msg.decode("ascii"))
-        else:
-            decoded_dict = msg
-        self.from_dict(decoded_dict)
-        return self
-
-    def to_dict(self):
-        return self.__dict__
 
     def from_dict(self, d):
         for attr_name in self.__dict__:
