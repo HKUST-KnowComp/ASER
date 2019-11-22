21a86b9e0dee98eff8ba1a21959b4f4d8efc596a
seanliu96 <seanliu96@outlook.com>
2019-11-22 08:42:55 +0800

    fix(aser-pipe): fix a bug about prefix

--- aser/pipe/merge_kgs.py
+++ aser/pipe/merge_kgs.py
@@ -1,5 +1,7 @@
 import os
+import pickle
 from copy import copy, deepcopy
+from collections import defaultdict
 from aser.database.base import SqliteConnection, MongoDBConnection
 from aser.database.kg_connection import CHUNKSIZE
 from aser.database.kg_connection import EVENTUALITY_TABLE_NAME, EVENTUALITY_COLUMNS, EVENTUALITY_COLUMN_TYPES
@@ -13,19 +15,41 @@ log_path = "./.tmp.log"
 
 if __name__ == "__main__":
     logger = init_logger(log_file=log_path)
-    kg_paths = [os.path.join("/metadata/data/corpora/aser/database", kg) for kg in ["gutenberg", "nyt", "reddit", "subtitles", "wikipedia" , "yelp"]]
-    merged_kg_path = "/metadata/data/corpora/aser/database/all"
+    datasets = ["gutenberg", "nyt", "reddit", "subtitles", "wikipedia" , "yelp"]
+    # datasets = ["nyt_test"]
+    kg_paths = [os.path.join("/home/data/corpora/aser/database/core", dataset) for dataset in datasets]
+    # kg_paths = [os.path.join(r"D:\Workspace\ASER-core\data\database", dataset) for dataset in datasets]
+    prefixes_to_be_added = [os.path.join(dataset, "parsed")+os.sep for dataset in datasets]
+
+    merged_kg_path = "/home/data/corpora/aser/database/core/all"
+    # merged_kg_path = r"D:\Workspace\ASER-core\data\database\all"
     if not os.path.exists(merged_kg_path):
         os.mkdir(merged_kg_path)
+
+    eid2sids, rid2sids = defaultdict(list), defaultdict(list)
+    for kg_path, prefix_to_be_added in zip(kg_paths, prefixes_to_be_added):
+        logger.info("Connecting %s" % (os.path.join(kg_path, "eid2sids.pkl")))
+        with open(os.path.join(kg_path, "eid2sids.pkl"), "rb") as f:
+            for eid, sids in pickle.load(f).items():
+                eid2sids[eid].extend([prefix_to_be_added+sid for sid in sids])
+
+        logger.info("Connecting %s" % (os.path.join(kg_path, "rid2sids.pkl")))
+        with open(os.path.join(kg_path, "rid2sids.pkl"), "rb") as f:
+            for rid, sids in pickle.load(f).items():
+                rid2sids[rid].extend([tuple([prefix_to_be_added+x for x in sid]) for sid in sids])
+    logger.info("Storing inverted tables")
+    with open(os.path.join(merged_kg_path, "eid2sids.pkl"), "wb") as f:
+        pickle.dump(eid2sids, f)
+    with open(os.path.join(merged_kg_path, "rid2sids.pkl"), "wb") as f:
+        pickle.dump(rid2sids, f)
+    del eid2sids, rid2sids
+
     if db == "sqlite":
         merged_conn = SqliteConnection(os.path.join(merged_kg_path, "KG.db"), CHUNKSIZE)
     elif db == "mongoDB":
         merged_conn = MongoDBConnection(os.path.join(merged_kg_path, "KG.db"), CHUNKSIZE)
     else:
         raise NotImplementedError
-
-    eid2row = dict()
-    rid2row = dict()
 
     logger.info("Creating tables...")
     # create tables
@@ -39,9 +63,12 @@ if __name__ == "__main__":
             merged_conn.create_table(table_name, columns, column_types)
         except BaseException as e:
             raise e
-
+    
+    eid2row = dict()
+    rid2row = dict()
     total_eventuality, total_relation = 0, 0
     for kg_path in kg_paths:
+        logger.info("Connecting %s" % (os.path.join(kg_path, "KG.db")))
         if db == "sqlite":
             conn = SqliteConnection(os.path.join(kg_path, "KG.db"), CHUNKSIZE)
         elif db == "mongoDB":
@@ -70,10 +97,12 @@ if __name__ == "__main__":
     logger.info("%d eventualities (%d unique) have been extracted." % (total_eventuality, len(eid2row)))
     logger.info("%d relations (%d unique) have been extracted." % (total_relation, len(rid2row)))
     
-    logger.info("Storing inverted tables and building the KG.")
+    logger.info("Building the KG.")
     merged_conn.insert_rows(EVENTUALITY_TABLE_NAME, eid2row.values())
     merged_conn.insert_rows(RELATION_TABLE_NAME, rid2row.values())
     merged_conn.close()
+    del eid2row, rid2row
+
     logger.info("Done.")
     close_logger(logger)
 
