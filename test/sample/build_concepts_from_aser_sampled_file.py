62bad3ee5c44d8329b31ef0300fbc9c764b40580
scarletpan <myscarlet@sina.com>
2020-02-25 16:44:10 +0800

    feat(concept): Replace finding entity with 's' or 'o' pattern by using POS tags

---
+++ test/sample/build_concepts_from_aser_sampled_file.py
@@ -0,0 +1,33 @@
+import sys
+import json
+from tqdm import tqdm
+from aser.eventuality import Eventuality
+from aser.concept.concept_extractor import ASERConceptExtractor
+
+
+if __name__ == "__main__":
+    in_event_file = sys.argv[1]
+    out_concept_file = sys.argv[2]
+    probase_path = sys.argv[3]
+
+    with open(in_event_file) as f:
+        all_records = json.load(f)
+
+    concept_extractor = ASERConceptExtractor(
+        method="probase",
+        probase_path=probase_path,
+        probase_topk=5)
+
+    for pattern, records in all_records.items():
+        print("{}:\n".format(pattern))
+        for record in tqdm(records):
+            concept_list = list()
+            for e_str in record["eventualities"]:
+                e = Eventuality().decode(json.loads(e_str), encoding=None)
+                concepts = concept_extractor.conceptualize(e)
+                c_strs = [(c.to_str(), score) for c, score in concepts]
+                concept_list.append(c_strs)
+            record["concepts"] = concept_list
+
+    with open(out_concept_file, "w") as f:
+        json.dump(all_records, f)
